5a368b2b1dd8cbe2ce76a3cafeacd92b0014cb28
GoodGuyGregory <greg.witt625@gmail.com>
2025-06-11 22:14:32 -0700

    adds notes about the main.rs to the project

--- src/main.rs
+++ src/main.rs
@@ -25,15 +25,19 @@ use microbit_bsp::{
 };
 use num_traits::float::FloatCore;
 
+// RGB LEVELS: Mutex to indicate the desired level to modify
 pub static RGB_LEVELS: Mutex<ThreadModeRawMutex, [u32; 3]> = Mutex::new([0; 3]);
+// Mutex to hold the frame rate of the UI State Component
 pub static FRAME_RATE: Mutex<ThreadModeRawMutex, u64> = Mutex::new(100);
 pub const LEVELS: u32 = 16;
 
+// accessor to the RGB_LEVELS value for ui reference and access
 async fn get_rgb_levels() -> [u32; 3] {
     let rgb_levels = RGB_LEVELS.lock().await;
     *rgb_levels
 }
 
+// setter method to set the RGB level for the UiState
 async fn set_rgb_levels<F>(setter: F)
 where
     F: FnOnce(&mut [u32; 3]),
@@ -42,11 +46,13 @@ where
     setter(&mut rgb_levels);
 }
 
+// accessor method to get the frame_rate from the UiState 
 async fn get_frame_rate() -> u64 {
     let frame_rate = FRAME_RATE.lock().await;
     *frame_rate
 }
 
+// setter used to modify the frame rate within the UiState
 async fn set_frame_rate(incoming_rate: u64)
 {
     let mut frame_rate = FRAME_RATE.lock().await;
@@ -68,6 +74,9 @@ async fn main(_spawner: Spawner) -> ! {
     let blue = led_pin(AnyPin::from(board.p16));
     let rgb: Rgb = Rgb::new([red, green, blue], 100);
 
+    // This saadc configures the Analog to Digital Signal source flow 
+    // this is needed because the knob has analog values which require 
+    // digital references to be represented in our state from 0-14BIT
     let mut saadc_config = saadc::Config::default();
     saadc_config.resolution = saadc::Resolution::_14BIT;
     let saadc = saadc::Saadc::new(
@@ -76,9 +85,16 @@ async fn main(_spawner: Spawner) -> ! {
         saadc_config,
         [saadc::ChannelConfig::single_ended(board.p2)],
     );
+
+    // configure the knob to leverage embassy style interrupts 
+    // this registers the interrupt handler from embassy to work with 
+    // the knob to trigger interrupts when turning 
     let knob = Knob::new(saadc).await;
+    // creates the ui object we will us to represent the UserInterface 
     let mut ui = Ui::new(knob, board.btn_a, board.btn_b);
 
+    // join allows both the RGB and UI states to run together and rely on eachother
+    // for execution.
     join::join(rgb.run(), ui.run()).await;
 
     panic!("fell off end of main loop");
